f8d8e9f958adc91b7ac2f3ecb3a0e94fce3e499f
kisnahc.github.io <dev.kisnahc@gmail.com>
2021-11-24 16:07:34 +0900

    Initial commit

---
+++ src/main/java/com/kisnahc/hostmonitoring/HostMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.kisnahc.hostmonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostMonitoringApplication.class, args);
+	}
+
+}
